3183abd8df40b141d7b428990a4bb35bbd06e331
Aadil Mansoor <aadilmansoor.am@gmail.com>
2024-05-14 20:45:46 +0530

    edit service section

--- components/OurServices.tsx
+++ components/OurServices.tsx
@@ -1,13 +1,17 @@
+import { services } from "@/constants";
 import ServiceCard from "./ServiceCard";
 
 const OurServices = () => {
   return (
     <section id="our-services">
-      <div className="flex flex-col items-center">
-        <h2 className="text-4xl font-medium text-primary">Our Services</h2>
+      <h2 className="text-4xl font-medium text-primary ms-5 sm:text-center sm:ms-0">
+        Our Services
+      </h2>
+      <div className="flex-center">
         <div className="flex justify-around max-w-[1200px] w-full mt-12 flex-wrap">
-          <ServiceCard />
-          <ServiceCard />
+          {services.map((service) => {
+            return <ServiceCard service={service} key={service.title} />;
+          })}
         </div>
       </div>
     </section>
--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -1,27 +1,33 @@
 import Image from "next/image";
 
-const ServiceCard = () => {
+type ServiceCardProps = {
+  service: {
+    title: string;
+    image: string;
+    description: string;
+  };
+};
+
+const ServiceCard = ({ service }: ServiceCardProps) => {
   return (
     <div className="relative group w-[100%] h-[550px] sm:w-[24rem] sm:h-[550px] sm:mx-8 mx-5 my-10 rounded-xl hover:scale-105  transition-all duration-500 overflow-hidden">
       <div className="absolute top-0 left-0 w-full h-full rounded-[inherit]">
         <Image
-          src="/assets/images/service1.png"
+          src={service.image}
           alt="background"
           className="rounded-[inherit] w-full h-full object-cover md:group-hover:scale-125 transition-all duration-500"
           width={640}
           height={550}
         />
       </div>
-      <div className="w-full h-full bg-[#0000008e]  md:bg-[#00000045] absolute top-0 left-0 z-50 md:group-hover:bg-[#0000008e] rounded-[inherit] transition-all duration-500">
+      <div className="w-full h-full bg-[#000000a1] md:bg-[#0000006c] absolute top-0 left-0 z-10 md:group-hover:bg-[#000000a1] rounded-[inherit] transition-all duration-500">
         <div className="absolute md:translate-y-[127px] bottom-0 mb-5 mx-4 group-hover:translate-y-0 transition-all duration-500">
           <h1 className=" text-white font-extrabold text-3xl mb-3">
-            Oil and Gas Equipment Trading
+            {service.title}
           </h1>
-          <div className="h-[5px] md:w-0 mb-3 md:opacity-0 bg-yellow-500 md:group-hover:w-[120px] md:group-hover:opacity-100 transition-all duration-700"></div>
+          <div className="h-[5px] w-[40%] md:w-0 mb-3 md:opacity-0 bg-yellow-500 md:group-hover:w-[120px] md:group-hover:opacity-100 transition-all duration-700"></div>
           <p className="text-white md:opacity-0 group-hover:opacity-100">
-            Explore our comprehensive range of equipment and materials for the
-            oil and gas sector, including drilling equipment, pipelines, valves,
-            pumps, and more.
+            {service.description}
           </p>
         </div>
       </div>
